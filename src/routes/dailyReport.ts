e1f71d9b57904dcad07245293e1d44e570717e62
gaurab-tango <techyboygaurab@gmail.com>
2025-12-16 23:14:58 +0530

    Update dailyReport.ts

--- src/routes/dailyReport.ts
+++ src/routes/dailyReport.ts
@@ -44,8 +44,8 @@ router.post("/daily-report", async (req, res): Promise<any> => {
   try {
     await dbConnect();
 
-   const DATE = new Date();
-   // const DATE = new Date(2025, 11, 3); //dec 3
+  // const DATE = new Date();
+    const DATE = new Date(2025, 11, 15); //dec 3
  // Fixed date for testing
    // const DATE = new Date(2025, 10, 26);
     // Add 5 hours 30 minutes to get IST
